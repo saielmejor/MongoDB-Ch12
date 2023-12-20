1082f280fb8deebb599d540a92e89424fddd1eb4
SAIKEN <SAIKEN@DESKTOP-HQRQLHN>
2023-12-20 17:38:54 -0500

    added edit product capability

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,9 @@
+
+const mongodb=require('mongodb') 
 const Product = require("../models/product");
 
+//request the object id 
+const ObjectId=mongodb.ObjectId; 
 exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
@@ -13,9 +17,11 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product=new Product(title, price, description, imageUrl)// save it as a constructor 
-  product.save().then(result => {
-      console.log(result)
+  const product = new Product(title, price, description, imageUrl); // save it as a constructor
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
       console.log("Created Product");
       res.redirect("/admin/products");
     })
@@ -24,3 +30,55 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId).then((product) => { 
+
+    if(!product){ 
+      return res.redirect('/')
+    }
+    res.render('admin/edit-product',{ 
+      pageTitle:'Edit Product', 
+      path:'/admin/edit-product', 
+      editing:editMode, 
+      product:product
+    })
+  }).catch(err=>{ 
+    console.log(err)
+  });
+};
+
+exports.postEditProduct=(req,res,next)=>{ 
+const prodId=req.body.productId; 
+const updateTitle=req.body.title; 
+const updatedPrice=req.body.price; 
+const updatedImageUrl=req.body.imageUrl; 
+const updatedDesc=req.body.description; 
+
+const product=new Product(updateTitle, updatedPrice,updatedDesc,updatedImageUrl,new ObjectId(prodId)) 
+
+product.save().then(result=>{ 
+  console.log('UPDATED PRODUCT') ; 
+  res.redirect('/admin/products')
+}).catch(err=>{ 
+  console.log(err)
+})
+}
